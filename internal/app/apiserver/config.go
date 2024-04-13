1ec80b79f6e03b3b5019f1ad1faa69b0ba162d56
vinc3r3 <kozhikovddd@gmail.com>
2024-04-13 18:26:07 +0500

    qazaqzha text

--- internal/app/apiserver/config.go
+++ internal/app/apiserver/config.go
@@ -82,7 +82,7 @@ func LoadData() {
 		}{
 			{
 				ID: 1,
-				Text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+				Text: "Қайырлы, Тарбағатай! Сен тау болып жаралғалсаң, құтты қоныс қойнауың мал мен жанға талай толып, талай солды ғой. Сенің еңбек оқиғалары шыққан сөздеріңде, адам өміріне көрсетіп берген нәтижелерді сен қалай білесің? Біз білмейміз деп ойлайсың, қай бола аламыз? Қазақ дауысындағы көңілді қара түнек Қаратау алдында тарих қойылды, қарым-қатынасымен шамырады тау - сен оларды шығарасың, Тарбағатай! Бір халқтың екі түрлі кездесуін көрсетіп алып, онда жерге түсіріп жатырсың, қылықтығым. Бақыт пен сортың арасын салықпен бөліп, туған еліңнің шығысындағы бір жетекшілік болып, осы түрде менің жүріп алған жолдарымды түзу қайырымсыз емес, қайырлы алтын адам!",
 				Questions: []struct {
 					ID       int      `json:"id"`
 					Question string   `json:"question"`
@@ -91,23 +91,23 @@ func LoadData() {
 				}{
 					{
 						ID: 1,
-						Question: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+						Question: "Қандай сипаттама Қаратауға қатысты айтылған?",
 						Options: []string{
-							"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-							"Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-							"Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-							"Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+							"Қазақтың қара шаңырағы.",
+							"Қтты қоныс қойнауы.",
+							"Туған елдің шығыстағы қамалы.",
+							"Бір халықг екі тҥрлі тағдыры бар.",
 						},
 						Correct: 0,
 					},
 					{
 						ID: 2,
-						Question: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+						Question: "Автор Тарбағатай тауын қалай қабылдайды?",
 						Options: []string{
-							"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-							"Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-							"Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-							"Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+							"Табиғаттың ерекше сыйы ретінде.",
+							"Жастық шағының куҽсі ретінде.",
+							"Манғаздықтан, асқақтықтың символы ретінде.",
+							"Балалық шағының куҽсі ретінде.",
 						},
 						Correct: 1,
 					},
